4e4d09d7a7b646ca248254efebc2dc90e0d26d65
Kyle Day <day00090@morris.umn.edu>
2021-10-21 11:14:19 -0500

    Update EchoServer.java with New Code
    This commit essentially reworks EchoServer.java with new code for intaking and displaying bytes from a client. The goal is not to simply just display a date.

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -3,33 +3,35 @@ import java.net.*;
 import java.io.*;
 
 public class EchoServer {
-    public static final int portNumber = 6013;
+  public static final int portNumber = 6013;
 
-    public static void main(String[] args) {
-      try {
-        // Start listening on the specified port
-        ServerSocket sock = new ServerSocket(portNumber);
-  
-        // Run forever, which is common for server style services
-        while (true) {
-          // Wait until someone connects, thereby requesting a date
-          Socket client = sock.accept();
-          System.out.println("Got a request!");
-  
-          // Construct a writer so we can write to the socket, thereby
-          // sending something back to the client.
-          PrintWriter writer = new PrintWriter(client.getOutputStream(), true);
-  
-          // Send the current date back tothe client.
-          writer.println(new java.util.Date().toString());
-  
-          // Close the client socket since we're done.
-          client.close();
+  public static void main(String[] args) {
+    try {
+      // Start listening on the specified port
+      ServerSocket server = new ServerSocket(portNumber);
+
+      // Run forever, which is common for server style services
+      while (true) {
+        // Wait until someone connects, thereby requesting a date
+        Socket client = server.accept();
+        System.out.println("Got a request!");
+
+        InputStream input = client.getInputStream();
+        OutputStream output = client.getOutputStream();
+
+        while(!client.isInputShutdown()) {
+            int sentByte = input.read();
+
+            output.write(sentByte);
+            output.flush();
         }
-      // *Very* minimal error handling.
-      } catch (IOException ioe) {
-        System.out.println("We caught an unexpected exception");
-        System.err.println(ioe);
+        
+        client.shutdownOutput();
       }
+    // *Very* minimal error handling.
+    } catch (IOException ioe) {
+      System.out.println("We caught an unexpected exception");
+      System.err.println(ioe);
     }
+  }
 }
