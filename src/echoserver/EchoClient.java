0fbab3cc18e088f04346b05c4edcc708aacb9f98
Kyle Day <day00090@morris.umn.edu>
2021-10-26 14:36:25 -0500

    Add Comments to EchoClient and EchoServer Java Files
    This commit adds comments to the EchoClient and EchoServer files to help readers understand the code. To assist with readability, the indentation of the while loops has been set in line with the rest of the code. Previously, they had been indented in further to assist with the writing of the code.

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -7,7 +7,8 @@ public class EchoClient {
 
     public static void main(String[] args) throws IOException {
       String server;
-      // Use "127.0.0.1", i.e., localhost, if no server is specified.
+      // Use "127.0.0.1", i.e., localhost, if no server is specified. Otherwise,
+      // use the server specified by the client.
       if (args.length == 0) {
         server = "127.0.0.1";
       } else {
@@ -20,23 +21,33 @@ public class EchoClient {
   
         // Get the input stream so we can read from that socket
         InputStream input = serverSocket.getInputStream();
+
+        // Get the output stream so we can write from that socket
         OutputStream output = serverSocket.getOutputStream();
   
         // Initialize the variable to contain bytes sent to the server.
         int sentByte = System.in.read();
 
+        // Facilitate the writing and re-sending of the bytes sent to the server back
+        // to the client.
         while((sentByte) != -1) {
-            output.write(sentByte);
+          // Write the byte taken in to the output stream for the socket.
+          output.write(sentByte);
             
-            int receivedByte = input.read();
-            System.out.write(receivedByte);
+          // Read a byte from the input stream and write it to the system's output.
+          int receivedByte = input.read();
+          System.out.write(receivedByte);
 
-            sentByte = System.in.read();
+          // Get the next byte being sent by the client.
+          sentByte = System.in.read();
         }
 
+        //Flush the stream for the system and the server socket outputs.
         System.out.flush();
         output.flush();
+        // Shutdown the socket's output.
         serverSocket.shutdownOutput();
+        // Close the socket.
         serverSocket.close();
   
       // Provide some minimal error handling.
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -15,20 +15,29 @@ public class EchoServer {
         // Wait until someone connects, thereby requesting a date
         Socket clientSocket = serverSocket.accept();
         System.out.println("Got a request!");
-
+        
+        // Get the input stream from the client's socket so that we can read from it
         InputStream input = clientSocket.getInputStream();
+
+        // Get the output stream of the client's socket so that we can write to it
         OutputStream output = clientSocket.getOutputStream();
 
+        // A variable for storing the bytes sent
         int sentByte;
 
+        // Facilitate the writing of the bytes sent and received to the client's output
+        // stream
         while((sentByte = input.read()) != -1) {
-
-            output.write(sentByte);
-            output.flush();
+          // Write a byte to the output
+          output.write(sentByte);
+          // Flush the output after each write.
+          output.flush();
         }
         
         System.out.println("The request has been processed.");
+        // Shutdown the output of the client socket so that no more bytes can be written to it
         clientSocket.shutdownOutput();
+        // Close the client socket completely
         clientSocket.close();
       }
     // *Very* minimal error handling.
